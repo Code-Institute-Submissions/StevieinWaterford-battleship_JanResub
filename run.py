0a7f8aa9efdb4e88ee1def3f95e62b82c799dce9
StevieinWaterford <stevieindub@gmail.com>
2021-08-01 12:19:40 +0000

    Add rendered board

--- run.py
+++ run.py
@@ -1,8 +1,17 @@
 # python code goes here
-inp = input("Where do do you want to shoot?")
-xstr, ystr = inp.split(" ,")
-x = int(xstr)
-y = int(ystr)
-print(x)
-print(y)
-print(x + y)
+
+def render(board_width, board_height):
+    print("+" + "-" * board_width + "+")
+
+    for i in range (board_height):
+        print("|" + " " * board_width + "|")
+
+if __name__ == "__main__":
+    render(10, 10)
+    inp = input("Where do do you want to shoot?\n")
+    xstr, ystr = inp.split(" ,")
+    x = int(xstr)
+    y = int(ystr)
+    print(x)
+    print(y)
+    print(x + y)
