709724995efd7579059c9a7e3166023f3fb2b518
StevieinWaterford <stevieindub@gmail.com>
2021-08-01 11:15:32 +0000

    Add correction to last commit , coordinates now validated as integers

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 # python code goes here
 inp = input("Where do do you want to shoot?")
-x, y = inp.split(" ,")
+xstr, ystr = inp.split(" ,")
+x = int(xstr)
+y = int(ystr)
 print(x)
 print(y)
+print(x + y)
