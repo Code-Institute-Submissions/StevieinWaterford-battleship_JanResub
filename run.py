5843bf2e91358a4fc7287930952a5e1285f51a98
StevieinWaterford <stevieindub@gmail.com>
2021-08-03 20:22:05 +0000

    Add input and coordinates on rendered board

--- run.py
+++ run.py
@@ -1,17 +1,33 @@
 # python code goes here
 
-def render(board_width, board_height):
-    print("+" + "-" * board_width + "+")
+def render(board_width, board_height, shots):
+    header = "+" + "-" * board_width + "+"
+    print(header)
+    
+    shots_set = set(shots)
+    for y in range(board_height):
+        row = []
+        for x in range(board_width):
+            if (x,y) in shots_set:
+                ch = "X"
+            else:
+                ch = ""
+            row.append(ch)
+        print("|" + "".join(row) + "|")
+        # print("|" + " " * board_width + "|")
 
-    for i in range (board_height):
-        print("|" + " " * board_width + "|")
+    
+    print(header)
 
 if __name__ == "__main__":
-    render(10, 10)
-    inp = input("Where do do you want to shoot?\n")
-    xstr, ystr = inp.split(" ,")
-    x = int(xstr)
-    y = int(ystr)
-    print(x)
-    print(y)
-    print(x + y)
+    shots = []
+
+    while True:
+        inp = input("Where do do you want to shoot?\n")
+        xstr, ystr = inp.split(" ,")
+        x = int(xstr)
+        y = int(ystr)
+
+        shots.append((x,y))
+        render(10,10,shots)
+    
