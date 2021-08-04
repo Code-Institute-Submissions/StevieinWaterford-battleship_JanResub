08908577623af0963e427b0989851269d96f4edd
StevieinWaterford <stevieindub@gmail.com>
2021-08-04 13:44:55 +0000

    Add priny outof coordinates

--- run.py
+++ run.py
@@ -1,33 +1,7 @@
 # python code goes here
 
-def render(board_width, board_height, shots):
-    header = "+" + "-" * board_width + "+"
-    print(header)
-    
-    shots_set = set(shots)
-    for y in range(board_height):
-        row = []
-        for x in range(board_width):
-            if (x,y) in shots_set:
-                ch = "X"
-            else:
-                ch = ""
-            row.append(ch)
-        print("|" + "".join(row) + "|")
-        # print("|" + " " * board_width + "|")
+board = []
 
-    
-    print(header)
-
-if __name__ == "__main__":
-    shots = []
-
-    while True:
-        inp = input("Where do do you want to shoot?\n")
-        xstr, ystr = inp.split(" ,")
-        x = int(xstr)
-        y = int(ystr)
-
-        shots.append((x,y))
-        render(10,10,shots)
-    
+for i in range(0,10):
+    board.append(["0"]*10)
+print(board)
