a71f9c29a7de411076bb2a9c45f3664729a0f484
StevieinWaterford <stevieindub@gmail.com>
2021-08-09 14:53:29 +0000

    Add functionality to game

--- run.py
+++ run.py
@@ -6,14 +6,15 @@ from random import randint
 
 board = []
 
-for i in range(0,10):
-    board.append(["0"]*10)
+for i in range(0,5):
+    board.append(["0"]*5)
 
 print(board)
 
 def print_board(board):
     for i in board:
         print(*i)
+
 
 print("Let's play Battleship\n")
 print_board(board)
@@ -34,28 +35,41 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-guess_row = int(input("Guess Row:"))
-guess_col = int(input("Guess Col:"))
 
-print(ship_row)
-print(ship_col)
+guesses = 0
+while guesses < 4:
+    guess_row = int(input("Guess Row:"))
+    guess_col = int(input("Guess Col:"))
 
-"""
-Create conditions for the game in terms of hits , misses and scope of the coordinates
-"""
+    print(ship_row)
+    print(ship_col)
 
-
-if guess_row == ship_row and guess_col == ship_col:
-    print("You've hit a battlship!")
-
-else:
-    if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print ("Missed by a long shot ,that's not even in the ocean.")
+    """
+    Create conditions for the game in terms of hits , misses and scope of the coordinates
+    """
 
 
-    print("You've missed")
-    board[guess_row][guess_col] = "X"
-    print_board(board)
+    if guess_row == ship_row and guess_col == ship_col:
+        board[guess_row][guess_col] == "&"
+        print("You've hit a battlship!")
+        break
+
+    elif (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+        print("Missed by a long shot ,that's not even in the ocean.")
+
+    elif board[guess_row][guess_col] == "X":
+        print("You guessed that already")
+
+
+    else: 
+        print("You've missed")
+        board[guess_row][guess_col] = "X"
+        print_board(board)
+        guesses = guesses +1
+        if guesses == 4:
+            print("Game over")
+
+        
 
 
 
