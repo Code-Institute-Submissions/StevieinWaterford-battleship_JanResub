97a1cb866dc22902f715b9b5614bac81f96dabb5
StevieinWaterford <stevieindub@gmail.com>
2021-10-30 15:30:07 +0000

    Add game function

--- run.py
+++ run.py
@@ -38,34 +38,42 @@ def random_row(board):
 def random_col(board):
     return randint(0, len(board[0]) - 1)
 
+def game():
 
-ship_row = random_row(board)
-ship_col = random_col(board)
-guesses = 0
-while guesses < 4: 
-    guess_row = int(input("Guess Row: \n"))
-    guess_col = int(input("Guess Col: \n"))
-    print(ship_row)
-    print(ship_col)
+    ship_row = random_row(board)
+    ship_col = random_col(board)
+    guesses = 0
+    while guesses < 4: 
+        guess_row = int(input("Guess Row: \n"))
+        guess_col = int(input("Guess Col: \n"))
+        print(ship_row)
+        print(ship_col)
 
-    """
-    Create conditions for game
-    """
-    if guess_row == ship_row and guess_col == ship_col:
-        print("You've hit a battlship!")
-        break
-    elif (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4): 
-        print("Missed by miles ,that's not even close.")
-    elif board[guess_row][guess_col] == "X":
-        print("You guessed that already")
-    else:
-        print("You've missed")
-        board[guess_row][guess_col] = "X"
+        """
+        Create conditions for game
+        """
+        if guess_row == ship_row and guess_col == ship_col:
+            print("You've hit a battlship!")
+            break
+        elif (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4): 
+            print("Missed by miles ,that's not even close.")
+        elif board[guess_row][guess_col] == "X":
+            print("You guessed that already")
+        else:
+            print("You've missed")
+            board[guess_row][guess_col] = "X"
+            print_board(board)
+            
         print_board(board)
-        
-    print_board(board)
-    guesses = guesses + 1    
+        guesses = guesses + 1    
 
-if guesses == 4:
+
     print("Game over")
-    
+
+while True:
+    game()
+    play_again = input(f"Would you like to play again {NAME} y/n: \n")
+    if play_again == "n":
+        print("Goodbye!")
+        break
+        
