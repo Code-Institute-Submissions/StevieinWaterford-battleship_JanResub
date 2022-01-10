fb31c8a949be8d52311f9c960a6c3446dbca4768
StevieinWaterford <stevieindub@gmail.com>
2022-01-10 15:18:07 +0000

    Add Docstrings to explain functionality

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
+
 from random import randint
 
-"""
-Create board with coordinates
-"""
+'''Takes board coordinates
+Returns board.
+'''
 
 
 def build_board(coords):
@@ -10,6 +11,10 @@ def build_board(coords):
 
 
 build_board(5)
+
+'''Prints Board
+Returns board with functionality.
+'''
 
 
 def print_board(board):
@@ -21,58 +26,60 @@ board = build_board(5)
 print_board(board)
 print('Welcome to Battleship!!'
       'The object of the game is to sink the computers ships \n')
-NAME = input("Enter your name: \n")
-print(f"hello {NAME}")
-"""
-Create ship on the board
-"""
+NAME = input('Enter your name: \n')
+print(f'hello {NAME}')
+
+'''Creates ship on board
+Retuns position of ship on the board.
+'''
 
 
 def random_row(board):
     return randint(0, len(board) - 1)
 
 
-def random_col(board):
+def random_column(board):
     return randint(0, len(board[0]) - 1)
+
+'''Creates the conditions for the game
+Returns the method in how to play the game
+'''
 
 
 def game():
 
     ship_row = random_row(board)
-    ship_col = random_col(board)
+    ship_column = random_column(board)
     guesses = 0
     while guesses < 4:
         try:
-            guess_row = int(input("Guess Row: \n"))
-            guess_col = int(input("Guess Col: \n"))
+            guess_row = int(input('Guess Row: \n'))
+            guess_column = int(input('Guess Col: \n'))
         except ValueError:
-            print("You have to enter a number, plesae try again.")
+            print('You have to enter a number, plesae try again.')
             continue
         print(ship_row)
-        print(ship_col)
-
-        """
-        Create conditions for game
-        """
-        if guess_row == ship_row and guess_col == ship_col:
-            print("You've hit a battlship!")
+        print(ship_column)
+        if guess_row == ship_row and guess_column == ship_column:
+            print('You have hit a battlship!')
             break
-        elif (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print("Missed by miles ,that's not even close.")
-        elif board[guess_row][guess_col] == "X":
-            print("You guessed that already")
+        elif (guess_row < 0 or guess_row > 4) or\
+             (guess_column < 0 or guess_column > 4):
+            print('Missed by miles ,that is not even close.')
+        elif board[guess_row][guess_column] == 'X':
+            print('You guessed that already')
         else:
-            print("You've missed")
-            board[guess_row][guess_col] = "X"
+            print('You have missed')
+            board[guess_row][guess_column] = 'X'
             print_board(board)
 
         print_board(board)
         guesses = guesses + 1
-    print("Game over")
+    print('Game over')
 
 while True:
     game()
-    play_again = input(f"Would you like to play again {NAME} y/n: \n")
-    if play_again == "n":
-        print("Goodbye!")
+    play_again = input(f'Would you like to play again {NAME} y/n: \n')
+    if play_again.lower() == 'n':
+        print('Goodbye!')
         break
